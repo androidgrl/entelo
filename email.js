0f2597ec0877d0b1a9d37484e46e4d50a3e815eb
androidgrl <kawaharajamie@gmail.com>
2017-12-20 21:54:42 -0800

    Make Sendgrid API POST request
    Make Sendgrid API POST request using the "to",
    "subject", "body" values coming in from the request
    payload.
    
    The Sendgrid api token is stored as a secret with
    Webtask.io
    
    This works, the email is sent, although there is a socket
    timeout which probably means that some response needs
    to be sent.
    
    Next step is to refactor.

--- email.js
+++ email.js
@@ -1,5 +1,43 @@
 const request = require('request');
 
 module.exports = function(context, cb) {
-  cb(null, `${context.body.to}`);
+  to = context.body.to;
+  subject = context.body.subject;
+  body = context.body.body;
+
+  postBody = {
+    "personalizations": [
+      {
+        "to": [
+          {
+            "email": `${to}`
+          }
+        ],
+        "subject": `${subject}`
+      }
+    ],
+    "from": {
+      "email": "jamiekaw@comcast.net"
+    },
+    "content": [
+      {
+        "type": "text/plain",
+        "value": `${body}`
+      }
+    ]
+  };
+
+  postOptions = {
+    method: 'post',
+    json: true,
+    body: postBody,
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization': `Bearer ${context.secrets.SENDGRID_API_KEY}`
+    },
+    url: 'https://api.sendgrid.com/v3/mail/send'
+  };
+
+  request.post(postOptions, function (err, res, body) {
+  });
 };
