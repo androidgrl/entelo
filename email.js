32f45b99067d7d2934d98ff8d317a4c43077d013
androidgrl <kawaharajamie@gmail.com>
2017-12-21 10:44:12 -0800

    Refactor into two methods
    Breakup the logic into two methods, one to format the
    post data, another to send the post request.
    
    Next will be to fix the socket timeout, which is probably
    due to not providing an http response after posting email.

--- email.js
+++ email.js
@@ -1,43 +1,44 @@
 const request = require('request');
 
-module.exports = function(context, cb) {
-  to = context.body.to;
-  subject = context.body.subject;
-  body = context.body.body;
+const formatPostOptions = (context, cb) => {
+  return new Promise (function (fulfill, reject) {
+    to = context.body.to;
+    subject = context.body.subject;
+    body = context.body.body;
 
-  postBody = {
-    "personalizations": [
-      {
-        "to": [
-          {
-            "email": `${to}`
-          }
-        ],
+    postBody = {
+      "personalizations": [{
+        "to": [{"email": `${to}`}],
         "subject": `${subject}`
-      }
-    ],
-    "from": {
-      "email": "jamiekaw@comcast.net"
-    },
-    "content": [
-      {
+      }],
+      "from": {"email": "jamiekaw@comcast.net"},
+      "content": [{
         "type": "text/plain",
         "value": `${body}`
-      }
-    ]
-  };
+      }]
+    };
 
-  postOptions = {
-    method: 'post',
-    json: true,
-    body: postBody,
-    headers: {
-      'Content-Type': 'application/json',
-      'Authorization': `Bearer ${context.secrets.SENDGRID_API_KEY}`
-    },
-    url: 'https://api.sendgrid.com/v3/mail/send'
-  };
+    postOptions = {
+      method: 'post',
+      json: true,
+      body: postBody,
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${context.secrets.SENDGRID_API_KEY}`
+      },
+      url: 'https://api.sendgrid.com/v3/mail/send'
+    };
 
-  request.post(postOptions, function (err, res, body) {
+    fulfill({postOptions: postOptions, cb: cb});
   });
+};
+
+const postRequest = (hash) => {
+  request.post(hash.postOptions, function (err, res, body) {
+  });
+};
+
+module.exports = function(context, cb) {
+  formatPostOptions(context, cb)
+    .then(postRequest);
 };
